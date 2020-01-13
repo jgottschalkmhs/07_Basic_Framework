7e5d45327e90e482a227679ace5a4d2314a27344
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-01-14 12:30:01 +1300

    Update 01_framework_trial.py

--- 01_framework_trial.py
+++ 01_framework_trial.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from functools import partial   # To prevent buttons opening multiple windows
 
 
 # Remember to name classes using CamelCase
@@ -38,7 +39,37 @@ class UserInput:
 
     def help(self):
         print("You asked for help")
+        get_help = Help(self)
 
+class Help:
+    def __init__(self, partner):
+
+        background = "orange"
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        self.help_box = Toplevel()
+
+        self.help_frame = Frame(self.help_box, width=300, height=200, bg=background)
+        self.help_frame.grid()
+
+        how_heading = Label(self.help_frame, text="Help / Instructions", font="arial 10 bold", bg=background)
+        how_heading.grid(row=0)
+
+        self.help_text = Label(self.help_frame,
+                               text="Help text normally goes here",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.help_text.grid(column=0, row=1)
+
+        dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg="orange", font="arial 10 bold",
+                             command=partial(self.close_help, partner))
+        dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 # main routine
 if __name__ == "__main__":
